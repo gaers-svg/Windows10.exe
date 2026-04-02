84176b0faa270747ba2b3944fad14936ecd821a8
gaers-svg <204278030+gaers-svg@users.noreply.github.com>
2026-04-01 20:35:05 -0700

    Add pygame and subprocess functionality for gdifunction2

--- ishowthneeds.py
+++ ishowthneeds.py
@@ -3,6 +3,8 @@ from tkinter import messagebox
 import time
 import ctypes as c
 from playsound3 import playsound
+import pygame
+import subprocess
 
 user32 = c.windll.user32
 d=c.windll.user32.GetDC(0)
@@ -15,23 +17,33 @@ def music():
     playsound("https://raw.githubusercontent.com/gaers-svg/ishowthneeds/main/getyopoopycomputerouttaherebro.mp3")
 
 def gdifunction():
-    music()
     try:
         while True:
-            g.StretchBlt(d, 0, 0, width, height, d, 0, 0, width, height, 0x00660046)
+            # 0x00330008 = NOTSRCCOPY → invert the screen
+            g.StretchBlt(d, 0, 0, width, height, d, 0, 0, width, height, 0x00330008)
             time.sleep(0.02)
     except KeyboardInterrupt:
         print("Stopped safely")
+def gdifunction2():
+    try:
+        while True: 
+            subprocess.run(["python", "-c", "import ctypes as c; g=c.windll.gdi32;d=c.windll.user32.GetDC(0);[g.StretchBlt(d, 5, 5, 1910, 1070, d, 0, 0, 1920, 1080, 0x999999) for _ in range(10**1)]"])
+            while pygame.mixer.music.get_busy():
+                time.sleep(0.1)
 
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def open(question, answer):
     response = messagebox.askyesno(question, answer)
     if response:
         print("You chose to continue.")
-        response2 = messagebox.askyesno("ishowmalware", "this is your last chance, do you want to continue?")
+        response2 = messagebox.askyesno("ishowthneeds", "this is your last chance, do you want to continue?")
         if response2:
+            messagebox.showerror("ishowthneeds", "you made a huge mistake.")
             gdifunction()
-            messagebox.showerror("ishowmalware", "you made a huge mistake.")
+            gdifunction2()
+            music()
         else:
             print("wise choice")
 open("Warning", "This program has the full potential to destroy your computer. Do you want to continue? ")
