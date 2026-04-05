d4db45c19f7739edc803fca3f07de7bd949a82db
gaers-svg <204278030+gaers-svg@users.noreply.github.com>
2026-04-04 21:22:21 -0700

    Update ishowthneeds.py

--- ishowthneeds.py
+++ ishowthneeds.py
@@ -1,9 +1,15 @@
+from sys import exception
 import tkinter as tk
 from tkinter import messagebox
 import time
 import ctypes as c
-import pygame
 import subprocess
+import threading
+from playsound3 import playsound
+
+def play_sound():
+    playsound("https://github.com/gaers-svg/ishowthneeds/raw/refs/heads/main/getyopoopycomputerouttaherebro.mp3")
+
 
 user32 = c.windll.user32
 d=c.windll.user32.GetDC(0)
@@ -12,29 +18,33 @@ g=c.windll.gdi32
 width = user32.GetSystemMetrics(0)
 height = user32.GetSystemMetrics(1)
 
-
 def gdifunction():
+    
     try:
-        while True:
-            # 0x00330008 = NOTSRCCOPY → invert the screen
-            g.StretchBlt(d, 0, 0, width, height, d, 0, 0, width, height, 0x00330008)
-            time.sleep(0.02)
+        while True: 
+            g.StretchBlt(d, 5, 5, width, height, d, 0, 0, 1920, 1080, 0x00990066)
+            time.sleep(0.1)
     except KeyboardInterrupt:
-        print("Stopped safely")
+        print("67")
+    
+def gdifunction2():
+    try:
+        while True: 
+            subprocess.run(["python", "-c", "import ctypes as c; g=c.windll.gdi32;d=c.windll.user32.GetDC(0);[g.StretchBlt(d, 5, 5, " + str(width) + ", " + str(height) + ", d, 0, 0, 1920, 1080, 0x999999) for _ in range(10**1)]"])
+            time.sleep(0.5)
 
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except exception:
+        print(f"poopy computer outtaherebro")
 
 def open(question, answer):
     response = messagebox.askyesno(question, answer)
     if response:
         print("You chose to continue.")
-        response2 = messagebox.askyesno("ishowthneeds", "this is your last chance, do you want to continue?")
+        response2 = messagebox.askyesno("Last Warning", "This program will destroy your computer if you say yes. Do you want to continue?")
         if response2:
             messagebox.showerror("ishowthneeds", "you made a huge mistake.")
+            threading.Thread(target=play_sound, daemon=True).start()
             gdifunction()
-            subprocess.run(["python", "-c", "import ctypes as c; g=c.windll.gdi32;d=c.windll.user32.GetDC(0);[g.StretchBlt(d, 5, 5, 1910, 1070, d, 0, 0, 1920, 1080, 0x999999) for _ in range(10**1)]"])
         else:
             print("wise choice")
 open("Warning", "This program has the full potential to destroy your computer. Do you want to continue? ")
